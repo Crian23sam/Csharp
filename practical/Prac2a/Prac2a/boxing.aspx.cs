d6f660b652ee1ef5fecb0b500d01c992cd622027
Crian23sam <152302320+Crian23sam@users.noreply.github.com>
2024-10-07 14:06:26 +0530

    Add files via upload

---
+++ practical/Prac2a/Prac2a/boxing.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Prac2a
+{
+    public partial class boxing : System.Web.UI.Page
+    {
+        TextBox tbx1;
+        Button btn1;
+        Label lbl1;
+        Literal ltr;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            tbx1 = new TextBox();
+            btn1 = new Button();
+            btn1.Text = "Do Boxing & Unboxing";
+            lbl1 = new Label();
+
+            btn1.Click +=new EventHandler(btn1_Click);
+
+            this.form1.Controls.Add(new Literal { Text = "Enter a Number" });
+            this.form1.Controls.Add(tbx1);
+            ltr = new Literal();
+            ltr.Text = "</br></br>";
+            this.form1.Controls.Add(ltr);
+
+            this.form1.Controls.Add(btn1);
+            ltr = new Literal();
+            ltr.Text = "</br></br>";
+            this.form1.Controls.Add(ltr);
+
+            this.form1.Controls.Add(lbl1);
+        }
+
+        protected void btn1_Click(object sender, EventArgs e)
+        {
+            string Value = tbx1.Text;
+            int number;
+            bool isNumeric = int.TryParse(Value, out number);
+
+            if (isNumeric)
+            {
+                object boxedValue = number;
+
+                int UnboxedValue = (int)boxedValue;
+
+                lbl1.Text = "Boxed value:" + boxedValue + " Unboxed Value:" + UnboxedValue;
+            }
+            else
+            {
+                lbl1.Text = "please enter a valid number";
+            }
+        }
+    }
+}
---
+++ practical/Prac2b/Prac2b/addition_subtraction.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Prac2b
+{
+    public partial class addition_subtraction : System.Web.UI.Page
+    {
+        TextBox tbx1, tbx2;
+        Button btn1, btn2;
+        Label lbl1;
+
+        public delegate int ArithOper(int x, int y);
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            tbx1 = new TextBox();
+            tbx2 = new TextBox();
+            btn1 = new Button();
+            btn1.Text = "Add";
+            btn1.Click += new EventHandler(btn1_Click);
+            btn2 = new Button();
+            btn2.Text = "Subtract";
+            btn2.Click += new EventHandler(btn2_Click);
+            
+            lbl1 = new Label();
+
+            this.form1.Controls.Add(new Literal { Text = "Enter a number" });
+            this.form1.Controls.Add(tbx1);
+
+            this.form1.Controls.Add(new Literal { Text = "</br>Enter second Number" });
+            this.form1.Controls.Add(tbx2);
+            this.form1.Controls.Add(new Literal { Text = "</br></br>" });
+            this.form1.Controls.Add(btn1);
+            this.form1.Controls.Add(btn2);
+            this.form1.Controls.Add(new Literal { Text = "</br></br>" });
+            this.form1.Controls.Add(lbl1);
+        }
+
+        protected void btn1_Click(object sender, EventArgs e)
+        {
+            PerfOper((x,y)=>x+y);
+        }
+
+        protected void btn2_Click(object sender, EventArgs e)
+        {
+            PerfOper((x,y)=>x-y);
+        }
+
+        protected void PerfOper(ArithOper operation)
+        {
+            int num1;
+            int num2;
+            bool Num1Valid = int.TryParse(tbx1.Text, out num1);
+            bool Num2Valid = int.TryParse(tbx2.Text, out num2);
+
+            if (Num1Valid && Num2Valid)
+            {
+                int result = operation(num1, num2);
+                lbl1.Text = "Result:" + result;
+            }
+            else
+            {
+                lbl1.Text = "Please enter a valid Number";
+            }
+        }
+    }
+}
